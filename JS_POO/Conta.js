96ee37102f8121c723d8b23d53985f7ee6394c92
eusebiops14 <64171953+eusebiops14@users.noreply.github.com>
2021-10-01 19:45:49 -0300

    herança em js

--- JS_POO/Conta.js
+++ JS_POO/Conta.js
@@ -3,13 +3,30 @@ export class Conta {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+        
+    }
+
+    set cliente(novoValor) {
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }   
+    }
+    
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
     }
 
     sacar(valor) {
-        taxa = 1.1 * valor;
-        if(this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
+        
+        let taxa = 1;
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
         } 
     }
     
@@ -22,6 +39,10 @@ export class Conta {
     }
 
     transferir(valor,conta) {
+        if (this._tipo == "salario"){
+            return;
+        }
+
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
